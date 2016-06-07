8d1297f4fbe2b2d1d95732a6f7e42d0f99252dba
Troy Acosta <troyacosta@sbcglobal.net>
2016-06-06 22:47:50 -0500

    adds boiler plate, react, and react-dom

---
+++ scripts/components/lightening.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+class Lightening extends React.Component {
+  render() {
+    return  <div className="spokes">
+              <p>BROWN</p>
+              <p>LIGHTENING</p>
+            </div>
+  }
+};
+ 
+export default Lightening;
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Lightening from './components/lightening.js';
+
+ReactDOM.render (<Lightening />, document.body);
